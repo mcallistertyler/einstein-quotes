f066b755ff823645e0fc6d5756be3e0e22d53833
Tyler McAllister <tytheguy95@gmail.com>
2019-02-25 15:04:14 +0100

    Updates

---
+++ precache-manifest.50c9b2c1a3e011dcdf9ab44e0b6d6637.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "41594c055ad9abbf1c70",
+    "url": "/einstein-quotes/static/css/main.9caf08da.chunk.css"
+  },
+  {
+    "revision": "41594c055ad9abbf1c70",
+    "url": "/einstein-quotes/static/js/main.41594c05.chunk.js"
+  },
+  {
+    "revision": "c5d8aa49fe7e02a86e44",
+    "url": "/einstein-quotes/static/js/runtime~main.c5d8aa49.js"
+  },
+  {
+    "revision": "19e5db0a439fca8e31b3",
+    "url": "/einstein-quotes/static/css/2.d2ca9746.chunk.css"
+  },
+  {
+    "revision": "19e5db0a439fca8e31b3",
+    "url": "/einstein-quotes/static/js/2.19e5db0a.chunk.js"
+  },
+  {
+    "revision": "2384305ae7cdd4502cd39e3c3dc22e40",
+    "url": "/einstein-quotes/static/media/relativity-formulae.2384305a.svg"
+  },
+  {
+    "revision": "8631319f96f633a24ecc1a95600588d7",
+    "url": "/einstein-quotes/index.html"
+  }
+];
