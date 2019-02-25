b7da0be09d3960d97817a123c81fa895ddde7cc5
Tyler McAllister <tytheguy95@gmail.com>
2019-02-25 16:08:57 +0100

    Updates

--- precache-manifest.2653b32b5270e218f383174cd1d286b3.js
+++ precache-manifest.2653b32b5270e218f383174cd1d286b3.js
@@ -1,30 +1,34 @@
 self.__precacheManifest = [
   {
-    "revision": "41594c055ad9abbf1c70",
+    "revision": "89f756a25bb836438ec4",
     "url": "/einstein-quotes/static/css/main.9caf08da.chunk.css"
   },
   {
-    "revision": "41594c055ad9abbf1c70",
-    "url": "/einstein-quotes/static/js/main.41594c05.chunk.js"
+    "revision": "89f756a25bb836438ec4",
+    "url": "/einstein-quotes/static/js/main.89f756a2.chunk.js"
   },
   {
     "revision": "c5d8aa49fe7e02a86e44",
     "url": "/einstein-quotes/static/js/runtime~main.c5d8aa49.js"
   },
   {
-    "revision": "19e5db0a439fca8e31b3",
+    "revision": "3229129c157b1cf97518",
     "url": "/einstein-quotes/static/css/2.d2ca9746.chunk.css"
   },
   {
-    "revision": "19e5db0a439fca8e31b3",
-    "url": "/einstein-quotes/static/js/2.19e5db0a.chunk.js"
+    "revision": "3229129c157b1cf97518",
+    "url": "/einstein-quotes/static/js/2.3229129c.chunk.js"
   },
   {
     "revision": "2384305ae7cdd4502cd39e3c3dc22e40",
     "url": "/einstein-quotes/static/media/relativity-formulae.2384305a.svg"
   },
   {
-    "revision": "8631319f96f633a24ecc1a95600588d7",
+    "revision": "b0932b34feaeecc04eb16b0567b0d2bd",
+    "url": "/einstein-quotes/static/media/einstein-1.b0932b34.jpg"
+  },
+  {
+    "revision": "991f029d67f7680631fda114ce748dd7",
     "url": "/einstein-quotes/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/einstein-quotes/precache-manifest.50c9b2c1a3e011dcdf9ab44e0b6d6637.js"
+  "/einstein-quotes/precache-manifest.2653b32b5270e218f383174cd1d286b3.js"
 );
 
 workbox.clientsClaim();
