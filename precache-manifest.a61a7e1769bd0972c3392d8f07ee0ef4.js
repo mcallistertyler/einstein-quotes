22745c2c95112eac3823ac8da6e45f16fe2e610d
Tyler McAllister <tytheguy95@gmail.com>
2019-03-06 01:24:14 +0100

    Updates

---
+++ precache-manifest.a61a7e1769bd0972c3392d8f07ee0ef4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "10928d3146ee9ea5fff2",
+    "url": "/einstein-quotes/static/css/main.da2ca148.chunk.css"
+  },
+  {
+    "revision": "10928d3146ee9ea5fff2",
+    "url": "/einstein-quotes/static/js/main.10928d31.chunk.js"
+  },
+  {
+    "revision": "c5d8aa49fe7e02a86e44",
+    "url": "/einstein-quotes/static/js/runtime~main.c5d8aa49.js"
+  },
+  {
+    "revision": "45545b45e716ad8ecb02",
+    "url": "/einstein-quotes/static/css/2.d2ca9746.chunk.css"
+  },
+  {
+    "revision": "45545b45e716ad8ecb02",
+    "url": "/einstein-quotes/static/js/2.45545b45.chunk.js"
+  },
+  {
+    "revision": "2384305ae7cdd4502cd39e3c3dc22e40",
+    "url": "/einstein-quotes/static/media/relativity-formulae.2384305a.svg"
+  },
+  {
+    "revision": "3e8bda34190e873ae13fe521099bd2d1",
+    "url": "/einstein-quotes/static/media/einstein-1.3e8bda34.jpg"
+  },
+  {
+    "revision": "944b8ce358ab15a936f255ffc4c8c4eb",
+    "url": "/einstein-quotes/static/media/einstein-2.944b8ce3.jpg"
+  },
+  {
+    "revision": "e5d64e3d6b17db646573bf4601004894",
+    "url": "/einstein-quotes/static/media/einstein-3.e5d64e3d.jpg"
+  },
+  {
+    "revision": "b7a038201873a1cebc208d08fff90c3a",
+    "url": "/einstein-quotes/static/media/einstein-4.b7a03820.jpg"
+  },
+  {
+    "revision": "f9b354ed4e77db9bf73afcf9428ef9bb",
+    "url": "/einstein-quotes/static/media/einstein-5.f9b354ed.jpg"
+  },
+  {
+    "revision": "2adc4473c3894c6c7d7c7d443192355f",
+    "url": "/einstein-quotes/index.html"
+  }
+];
